c34a6bd0bd04f64cfdf815a90da0dc18ed7bd798
Lelle222 <161458991+Lelle222@users.noreply.github.com>
2024-08-02 12:05:54 +0200

    feat: register api function

--- src/features/auth/api/auth.ts
+++ src/features/auth/api/auth.ts
@@ -14,7 +14,26 @@ export async function login(username: string, password: string){
     );
 
     if (!response.ok) {
-      throw new Error('Login failed');
+      throw new Error('Login failed!');
+    }
+    return await response.json();
+}
+
+export async function register(username: string, password: string, confirm:string){
+    const response = await api.post(
+      '/auth/register',
+      {
+        method: 'POST',
+        body: JSON.stringify({ username, password, confirm }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      },
+      false
+    );
+
+    if (!response.ok) {
+      throw new Error('Register failed!');
     }
     return await response.json();
 }
