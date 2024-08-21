5392e4ad21002948d718898b3e33a96f61246e87
FabioAho <fabioarca.88@gmail.com>
2024-08-21 10:25:05 +0200

    test: add before in Authentication Test

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -16,10 +16,17 @@ describe('Page should change', () => {
 })
 
 describe('Authentication Test', () => {
+  before(() => {
+    cy.window().then((window) => {
+      expect(window.sessionStorage.length).to.equal(0);
+    });
+  });
+
   it('should redirect to login page if user is not logged in', () => {
     cy.visit('http://localhost:5173/');
-    cy.contains('Dashboard').click()
+    cy.contains('Dashboard').click();
     cy.url().should('include', '/login');
     cy.contains('Login').should('be.visible');
   });
 });
+
