256a42686823a24a0d97eaa08a88c958b522e632
Gianfranco Baracca <toph99beifong@gmail.com>
2024-08-23 09:48:25 +0200

    test: register cypress test

--- cypress/e2e/user_login.cy.ts
+++ cypress/e2e/user_login.cy.ts
@@ -1,19 +1,20 @@
+import { BASE_URL } from "../contants";
 
 describe('Page should change', () => {
-  it('should navigate from Landing page to Register page', () => {
-    cy.visit('http://localhost:5173/')
-    cy.contains('Register').click()
+  it('navigate from Landing page to Register page', () => {
+    cy.visit(BASE_URL);
+    cy.contains('Register').click();
     cy.url().should('include', '/register');
-  })
-})
+  });
+});
 
 describe('Page should change', () => {
-  it('should navigate from Landing page to Login page', () => {
-    cy.visit('http://localhost:5173/')
-    cy.contains('Login').click()
+  it('navigate from Landing page to Login page', () => {
+    cy.visit(BASE_URL);
+    cy.contains('Login').click();
     cy.url().should('include', '/login');
-  })
-})
+  });
+});
 
 describe('Authentication Test', () => {
   before(() => {
@@ -22,11 +23,10 @@ describe('Authentication Test', () => {
     });
   });
 
-  it('should redirect to login page if user is not logged in', () => {
-    cy.visit('http://localhost:5173/');
+  it('redirect to login page if user is not logged in', () => {
+    cy.visit(BASE_URL);
     cy.contains('Dashboard').click();
     cy.url().should('include', '/login');
     cy.contains('Login').should('be.visible');
   });
 });
-
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,31 +1,28 @@
-import { ProtectedPage } from "@/components/protected-page";
-import { LandingPage } from "@/pages/landing-page";
-import { LoginPage } from "@/pages/login/login-page";
-import { RegisterPage } from "@/pages/register/register-page";
-import { createBrowserRouter } from "react-router-dom";
+import { ProtectedPage } from '@/components/protected-page';
+import { LandingPage } from '@/pages/landing-page';
+import { LoginPage } from '@/pages/login/login-page';
+import { RegisterPage } from '@/pages/register/register-page';
+import { createBrowserRouter } from 'react-router-dom';
 
 export const router = createBrowserRouter([
   {
-    path: "/",
-    element: <LandingPage/>
+    path: '/',
+    element: <LandingPage />
   },
   {
-    path: "/login",
-    element: <LoginPage/>
-  },
-   {
-    path: "/register",
-    element: <RegisterPage/>
+    path: '/login',
+    element: <LoginPage />
   },
   {
-    path: "/register",
-    element: <RegisterPage/>
+    path: '/register',
+    element: <RegisterPage />
   },
-
   {
-    path: "/appointments",
-    element: <ProtectedPage>
-      <div>appointments</div>
-    </ProtectedPage>,
-  },
+    path: '/appointments',
+    element: (
+      <ProtectedPage>
+        <div>appointments</div>
+      </ProtectedPage>
+    )
+  }
 ]);
