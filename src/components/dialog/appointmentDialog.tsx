d8c54a2e2d79f97fb1c7dcdb8a453a9d499dd9ef
Lelle222 <161458991+Lelle222@users.noreply.github.com>
2024-09-30 11:00:55 +0200

    fix: dialog component

--- src/components/dialog/appointmentDialog.tsx
+++ src/components/dialog/appointmentDialog.tsx
@@ -1,44 +1,25 @@
-import { forwardRef, ReactNode, useImperativeHandle, useRef } from 'react';
+import { ReactNode } from 'react';
 import styles from './appointmentDialog.module.css';
-
-export type DialogRef = {
-  open: () => void;
-  close: () => void;
-};
 
 interface DialogProps {
   title: string;
+  isOpen: boolean; 
   onClose: () => void;
   children: ReactNode;
 }
 
-const Dialog = forwardRef<DialogRef, DialogProps>(({ title, onClose, children }, ref) => {
-  const dialogRef = useRef<HTMLDialogElement>(null);
-
-  useImperativeHandle(ref, () => ({
-    open() {
-      dialogRef.current?.showModal();
-    },
-    close() {
-      dialogRef.current?.close();
-    }
-  }));
-
-  const handleDialogClose = () => {
-    onClose();
-  };
-
+const Dialog = ({ title, isOpen, onClose, children }: DialogProps) => {
   return (
-    <dialog ref={dialogRef} className={styles.dialog} onClose={handleDialogClose}>
+    <dialog open={isOpen} className={styles.dialog} onClose={onClose}>
       <div className={styles.header}>
         <h2 className={styles.h2}>{title}</h2>
-        <button onClick={() => dialogRef.current?.close()} className={styles.closeButton}>
+        <button onClick={onClose} className={styles.closeButton}>
           X
         </button>
       </div>
       <div className={styles.body}>{children}</div>
     </dialog>
   );
-});
+};
 
 export default Dialog;
